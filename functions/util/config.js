894a2e2338f1249bd3f104fd76ddefcefb97675b
psstory <30810568+psstory@users.noreply.github.com>
2019-08-22 10:36:30 -0400

    remove config file

--- functions/util/config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  apiKey: 'AIzaSyDeuSp6WbEyymU7i79nFXSglCu7m5a0gmA',
-  authDomain: 'socialapp-91710.firebaseapp.com',
-  databaseURL: 'https://socialapp-91710.firebaseio.com',
-  projectId: 'socialapp-91710',
-  storageBucket: 'socialapp-91710.appspot.com',
-  messagingSenderId: '630527026168',
-  appId: '1:630527026168:web:364503d7b2b505b4'
-}
